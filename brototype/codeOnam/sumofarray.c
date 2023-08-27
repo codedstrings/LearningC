150fb93404c8300dd92de11356e834574202ff7c
Mridhul ML <69009760+codedstrings@users.noreply.github.com>
2023-08-27 12:21:42 +0530

    Update sumofarray.c

--- brototype/codeOnam/sumofarray.c
+++ brototype/codeOnam/sumofarray.c
@@ -1,1 +1,29 @@
+#include<stdio.h>
 
+int main(){
+    int arr[20],sum=0,i,n;
+    
+//input array size
+    printf("array size: ");
+    scanf("%d",&n);
+    
+//input array values
+    printf("Array values:\n");
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    
+//print array
+     for(i=0;i<n;i++){
+        printf("\n%d",arr[i]);
+     }
+     
+ //calculate sum
+    for(i=0;i<n;i++)
+        sum+=arr[i];
+    
+//printing sum
+    printf("\nsum=%d",sum);
+
+return 0;
+}
